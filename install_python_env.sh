af9b724bb82c18c1f675f78191231aa9f51e1710
Oleg Tarasov <tolegu@ya.ru>
2022-11-13 12:26:01 +0300

    initial commit

---
+++ install_python_env.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+sudo apt update
+
+
+# Устанавливаем Python 3.10
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+
+wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz
+
+tar -xzvf Python-3.10.8.tgz 
+
+cd Python-3.10.8/
+
+./configure --enable-optimizations --prefix=/home/www/.python3.10
+
+make -j 2
+
+sudo make altinstall
+
+rm -r Python-3.10.8
+
+rm Python-3.10.8.tgz
+
+
+# Устанавливаем PostgreSQL
+sudo apt -y install postgresql
+
+
+# Устанавливаем Redis
+sudo apt install redis
