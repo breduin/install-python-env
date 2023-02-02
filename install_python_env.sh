5fd25bfb2ada82e04c9aa6704fa828a60b4b8f25
Oleg Tarasov <tolegu@ya.ru>
2023-02-02 12:35:30 +0300

    Python 3.10.8 -> 3.10.9

--- install_python_env.sh
+++ install_python_env.sh
@@ -7,12 +7,13 @@ sudo apt update
 # Устанавливаем Python 3.10
 sudo apt -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
 
-wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz
+wget https://www.python.org/ftp/python/3.10.9/Python-3.10.9.tgz
 
-tar -xzvf Python-3.10.8.tgz 
+tar -xzvf Python-3.10.9.tgz 
 
-cd Python-3.10.8/
+cd Python-3.10.9/
 
+# опция --prefix позволяет пользователю запускать питон командой python3.10
 ./configure --enable-optimizations --prefix=/home/www/.python3.10
 
 make -j 2
@@ -21,9 +22,9 @@ sudo make altinstall
 
 cd ../
 
-rm -r Python-3.10.8
+rm -r Python-3.10.9
 
-rm Python-3.10.8.tgz
+rm Python-3.10.9.tgz
 
 
 # Устанавливаем PostgreSQL
